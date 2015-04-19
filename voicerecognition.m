aacd5c1a71070cafa34319e0c895f4cce6788fe5
tyx3gu <tyx3gu@virginia.edu>
2015-04-19 16:14:17 -0400

    More MFCC code

--- voicerecognition.m
+++ voicerecognition.m
@@ -1,41 +1,56 @@
-clear all;
-clc;
-
-bandwidth = 3500; % Bandwidth of human speech is about 3400 Hz
-Fs = 2*bandwidth; % Nyquist rate
-window = Fs/100; % Sample window
-
-input = analoginput('winsound'); % Set up sound card input
-addchannel(ai, 1);
-
-% Configure analog input
-set(ai, 'SampleRate', Fs);
-set(ai, 'SamplesPerTrigger', window);
-set(ai, 'TriggerRepeat', inf);
-set(ai, 'TriggerType', 'immediate');
-
-start(ai); % Start recording
-
-is_recording = 1;
-
-while is_recording
-    data = getdata(ai, ai, SamplesPerTrigger);
-    seglength = window;
-    overlap = seglength/2;
-    stepsize = seglength - overlap;
-    frames = length(data)/stepsize - 1;
+function features = MFCC(x, fs)
+    % Get a 25ms frame length
+    framelength = floor(.025*fs);
     
-    s1 = 1;
-    s2 = seglength;
-    
-    for i = 1:frames
-        currframe = data(s1:s2);
-        s1 = s1 + stepsize;
-        s2 = s2 + stepsize;
+    % Zero-pad signal with ones if size does not divide evenly
+    sizediff = mod(length(x),framelength);
+    if sizediff ~= 0
+        for n = 1:sizediff
+            x(end+1) = 0;
+        end
     end
-        
+    
+    % Take the 512-point DFT
+    S = fft(x, 512);
+    % Keep only the first 257 elements
+    S = S(1:257);
+    P = zeros(257);
+    for n = 1:257
+        P(n) = S(n)*S(n)./512;
+    end
+    
+    filterbank - get_filterbank(300, 8000, P, fs);
 end
 
-stop(ai);
-delete(ai);
-clear ai;
+function m = f2mel(freq)
+    m = 1125*ln(1+freq/700);
+end
+
+function f = mel2f(m)
+    f=700*(exp(m/1125)-1);
+end
+
+function filterbank = get_filterbank(lower, upper, P, fs)
+    % Upper and lower bounds in the Mel domain
+    lower_mel = f2mel(lower);
+    upper_mel = f2mel(upper);
+    
+    % Want to generate 26 additional points between upper and lower bounds
+    m_vector = linspace(lower_mel, upper_mel, 28);
+    h_vector = zeros(length(m_vector));
+    
+    for k = 1:length(m_vector)
+        h_vector(k) = mel2f(m_vector(k));
+    end
+    
+    f_bins = zeros(length(h_vector));
+    
+    % Sort frequencies into bins
+    for k = 1:length(h_vector)
+        f_bins(k) = floor((P+1)*h_vector(k)/fs);
+    end
+    filterbank = zeros(length(f_bins));
+    
+    
+    
+end    
